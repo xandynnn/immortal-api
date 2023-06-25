f2b5492be5efd952d30f08b552f78c3f9ec6cd75
Xandynnn <xandynnn@gmail.com>
2023-06-25 01:03:21 -0300

    refactor: Altera netlify functions

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-import serverless from 'serverless-http';
 const app = express();
 const port = 3000;
 
@@ -11,5 +10,3 @@ require('./controllers/setsController')(app);
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server running on port ${port}`);
 });
-
-export const handler = serverless(app);
